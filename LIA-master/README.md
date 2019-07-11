21d1b68bff6d7e7885275dcb32b048f2b48b8a81
Nicolas Perron <nicolas-2.perron@polymtl.ca>
2019-07-11 14:06:51 -0400

    Removed old directory

--- LIA-master/README.md
+++
@@ -1,3 +0,0 @@
-#Lab-Software
-Python program to move a PI stage while gathering measurements from a  ZI UHFLI Lock-in Amplifier. Allows control of the stage and of the LIA.
-Programed by Nicolas Perron.
--- LIA-master/zhinst/examples/hf2/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Zurich Instruments LabOne Python API Examples for the HF2 Lock-in Amplifier.
-"""
-import zhinst.examples.hf2.example_pid_advisor_pll
-import zhinst.examples.hf2.example_scope
-
-__all__ = ["example_pid_advisor_pll", "example_scope"]
-
-del zhinst
