c059b76b17bec62de7bf8957e5c5c9d87afcbeb8
UltraFastQ Lab <40898165+UltraFastQ@users.noreply.github.com>
2022-02-03 16:47:00 -0500

    corrected error in spectro message

--- Labo_Env/ultrafastGUI/Spectrometer.py
+++ Labo_Env/ultrafastGUI/Spectrometer.py
@@ -472,7 +472,7 @@ class Spectro:
             np.save('measurements/'+'Spectre_' +dt, array)
         except:
             messagebox.showinfo(title='ERROR', message='Create a folder'+
-                                ' named measurement inside this folder')
+                                ' named measurements inside this folder')
 
     def auto_update_fft(self, variable):
         """
