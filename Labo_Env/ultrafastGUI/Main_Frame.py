28894db8e51afdc5c14d26542a1cae07aac0c9af
bison-demontigny <gabriel.demontigny@outlook.com>
2022-02-03 14:21:22 -0500

    Update main frame with iHR320 experiment

--- Labo_Env/ultrafastGUI/Main_Frame.py
+++ Labo_Env/ultrafastGUI/Main_Frame.py
@@ -1997,6 +1997,8 @@ class Experiment(ttk.LabelFrame):
                              'Pump_Probe': ['Wavelengths [nm]', 'Delay [um]'], })
         create_layout(name='Batch Spectra', function_=Experiment_file.batchSpectra, option=['Spectrometer'],
                       graph={'Spectrometer': ['Wavelengths [nm]', 'Intensity [arb.u.]']})
+        create_layout(name='iHR320', function_=Experiment_file.iHR320, option=['Spectrometer'],
+                      graph={'Spectrometer': ['Wavelengths [nm]', 'Intensity [arb.u.]']})
 
 
         
